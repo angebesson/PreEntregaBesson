11870f8d28660da9d1a6db76bfa366b1284db030
Angelina Besson <formadeltiempoange@gmail.com>
2025-01-10 08:28:21 -0300

    adding categories to firebase

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -13,7 +13,9 @@ import {
 import { Link } from "react-router-dom";
 
 export const ItemListContainer = ({ products }) => {
-  
+  // const categories = products.map((product)=>product.category);
+  //  const uniqueCategories = [...new Set (categories)];
+  // console.log(uniqueCategories);
   return (
     <Box display={"flex"} flexWrap={"wrap"}  >
       {products.map((product) => (
